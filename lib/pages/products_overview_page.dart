3132588e4ecd2b0e8b6fa2e1d89cd9f65b901810
navas <gabrielnavas@email.com>
2024-02-26 23:36:49 -0300

    create product overview page with grid view

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flutter_app/models/product.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  const ProductsOverviewPage({super.key});
+  List<Product> loadedProducts = Product.generateProducts();
+
+  ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length, // amount item to render
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // two products peer line
+          childAspectRatio: 3 / 2, // 1.5
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
       ),
     );
   }
