cc7a74cc5137e39320130b27dc83853447508758
navas <gabrielnavas@email.com>
2024-02-27 11:53:01 -0300

    remove provider and back to product detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_flutter_app/pages/counter_page.dart';
+import 'package:shop_flutter_app/pages/product_detail_page.dart';
 import 'package:shop_flutter_app/pages/products_overview_page.dart';
-import 'package:shop_flutter_app/providers/counter.dart';
 import 'package:shop_flutter_app/routes.dart';
 
 void main() {
@@ -14,19 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'shop app flutter',
-        theme: ThemeData(
-          useMaterial3: true,
-          fontFamily: 'Lato',
-        ),
-        routes: {
-          Routes.productDetail: (_) => const CounterPage(),
-        },
-        home: ProductsOverviewPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'shop app flutter',
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: 'Lato',
       ),
+      routes: {
+        Routes.productDetail: (_) => const ProductDetailPage(),
+      },
+      home: ProductsOverviewPage(),
     );
   }
 }
