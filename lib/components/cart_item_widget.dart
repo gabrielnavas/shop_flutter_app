7b6f8d6c4a381e75d7e218766847e20fe25c3ec2
navas <gabrielnavas@email.com>
2024-02-29 15:58:07 -0300

    add shield to remove product from cart

--- lib/components/cart_item_widget.dart
+++ lib/components/cart_item_widget.dart
@@ -15,6 +15,33 @@ class CartItemWidget extends StatelessWidget {
     return Dismissible(
       key: ValueKey(cartItem.id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (_) {
+        return showDialog<bool>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('Tem certeza?'),
+            content: const Text('Quer remover o item do carrinho?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: const Text(
+                  'Não',
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: const Text(
+                  'Sim',
+                ),
+              )
+            ],
+          ),
+        );
+      },
       onDismissed: (_) {
         Provider.of<Cart>(
           context,
