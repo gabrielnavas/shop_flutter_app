a2176f8371ca0107afeb6889fc04d464ce4ea05f
navas <gabrielnavas@email.com>
2024-02-29 17:56:10 -0300

    create form add product

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
+class ValidateAttribute {
+  final String attribute;
+  final String message;
+
+  ValidateAttribute(this.attribute, this.message);
+}
+
 class Product with ChangeNotifier {
-  final String id;
-  final String name;
-  final String description;
-  final double price;
-  final String imageUrl;
+  String id;
+  String name;
+  String description;
+  double price;
+  String imageUrl;
   bool isFavorite;
 
   Product({
@@ -16,6 +23,38 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
+
+  static Product init() {
+    return Product(id: '', name: '', description: '', price: 0.0, imageUrl: '');
+  }
+
+  static String? validName(String name) {
+    if (name.isEmpty) {
+      return 'O nome está vazio.';
+    }
+    return null;
+  }
+
+  static String? validDescription(String description) {
+    if (description.isEmpty) {
+      return 'A descrição está vazia.';
+    }
+    return null;
+  }
+
+  static String? validPrice(double price) {
+    if (price < 0.00) {
+      return 'Preço está negativo.';
+    }
+    return null;
+  }
+
+  static String? validImageUrl(String imageUrl) {
+    if (imageUrl.isEmpty) {
+      return 'A url da imagem está vazia.';
+    }
+    return null;
+  }
 
   void toggleFavorite() {
     isFavorite = !isFavorite;
--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flutter_app/models/product.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -8,11 +9,152 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final TextEditingController _imageUrlController = TextEditingController();
+  final _nameFocus = FocusNode();
+  final _descriptionUrlFocus = FocusNode();
+  final _priceUrlFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final Product product = Product.init();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameFocus.dispose();
+    _descriptionUrlFocus.dispose();
+    _priceUrlFocus.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+    _imageUrlFocus.dispose();
+  }
+
+  void updateImage() {
+    // it's update the _imageUrlController, because if
+    // _imageUrlController.text is empty, need show image to text
+    // and it's update the UI
+    setState(() {});
+  }
+
+  void _submitForm() {
+    final bool isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState?.save();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de produto'),
+        actions: [
+          IconButton(
+            onPressed: () => _submitForm(),
+            icon: const Icon(
+              Icons.save,
+              color: Colors.blueAccent,
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                  decoration: const InputDecoration(label: Text('Nome')),
+                  focusNode: _nameFocus,
+                  textInputAction: TextInputAction.next,
+                  autofocus: true,
+                  validator: (value) => Product.validName(value ?? ''),
+                  onSaved: (name) {
+                    if (name != null && name.isNotEmpty) {
+                      product.name = name;
+                    }
+                  }),
+              TextFormField(
+                  decoration: const InputDecoration(label: Text('Preço')),
+                  focusNode: _priceUrlFocus,
+                  textInputAction: TextInputAction.next,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  validator: (value) {
+                    try {
+                      double price = double.parse(value ?? '0');
+                      return Product.validPrice(price);
+                    } catch (ex) {
+                      return 'Digite somente números';
+                    }
+                  },
+                  onSaved: (price) {
+                    if (price != null && price.isNotEmpty) {
+                      product.price = double.parse(price);
+                    }
+                  }),
+              TextFormField(
+                  decoration: const InputDecoration(label: Text('Descrição')),
+                  focusNode: _descriptionUrlFocus,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  validator: (value) => Product.validDescription(value ?? ''),
+                  onSaved: (description) {
+                    if (description != null && description.isNotEmpty) {
+                      product.description = description;
+                    }
+                  }),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Url da imagem')),
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.url,
+                        controller: _imageUrlController,
+                        focusNode: _imageUrlFocus,
+                        onFieldSubmitted: (value) => _submitForm(),
+                        validator: (value) =>
+                            Product.validImageUrl(value ?? ''),
+                        onSaved: (imageUrl) {
+                          if (imageUrl != null && imageUrl.isNotEmpty) {
+                            product.imageUrl = imageUrl;
+                          }
+                        }),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(top: 10, left: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(136, 130, 130, 130),
+                          width: 1),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? const Text('informe a url')
+                        : FittedBox(
+                            fit: BoxFit.cover,
+                            child: Image.network(_imageUrlController.text),
+                          ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
